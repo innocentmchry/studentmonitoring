c9454873167706f935085f4d14694918e15f0428
Innocent Mochahari <imdengkhw@gmail.com>
2024-03-13 00:56:40 +0530

    revert previous changes

--- mychat/deployment.py
+++ mychat/deployment.py
@@ -19,7 +19,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
