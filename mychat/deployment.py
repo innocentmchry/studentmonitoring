96184d29758f2aea6137e858ef9a711c4df725ea
Innocent Mochahari <imdengkhw@gmail.com>
2024-03-13 02:53:53 +0530

    trying to resolve collectstatic

--- mychat/deployment.py
+++ mychat/deployment.py
@@ -19,7 +19,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
