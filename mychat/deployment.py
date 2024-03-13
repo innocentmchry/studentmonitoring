156229f4cbc424dc925e787898f0603b544bb16a
Innocent Mochahari <imdengkhw@gmail.com>
2024-03-13 14:29:59 +0530

    trying to hardcode postgres connection

--- mychat/deployment.py
+++ mychat/deployment.py
@@ -23,14 +23,14 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "sm",
+        "USER": "evihjczmcp",
+        "PASSWORD": "Dengkhw273@",
+        "HOST": "studentmonitoring-server.postgres.database.azure.com",
+        "PORT": "5432",
+        "OPTIONS": {"sslmode": "require"},
     }
 }
